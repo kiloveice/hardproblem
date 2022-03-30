a48f0c3f5e03fdcf67a90ee3435d7bb60165cb3e
iloice <2869683143@qq.com>
2022-03-30 20:18:36 +0800

    项目初始化

---
+++ src/main/java/live/hardproblem/HardproblemApplication.java
@@ -0,0 +1,13 @@
+package live.hardproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardproblemApplication.class, args);
+	}
+
+}
