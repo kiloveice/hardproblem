cd9a20b112f9197646201e21c69b605a80c74012
ice <1378430147@qq.com>
2022-04-04 13:51:44 +0800

    新增菜单、更新菜单

--- src/main/java/live/hardproblem/controller/MenuController.java
+++ src/main/java/live/hardproblem/controller/MenuController.java
@@ -1,14 +1,16 @@
 package live.hardproblem.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import live.hardproblem.beans.HttpResponseEntity;
 import live.hardproblem.dao.entity.Menu;
+import live.hardproblem.dao.entity.Tag;
 import live.hardproblem.service.MenuService;
+import live.hardproblem.util.IpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 @Slf4j
@@ -17,6 +19,9 @@ import java.util.ArrayList;
 public class MenuController {
     @Autowired
     MenuService menuService;
+    //jackson json工具
+    @Autowired
+    ObjectMapper mapper;
 
     @GetMapping("/")
     public HttpResponseEntity getAll() {
@@ -25,6 +30,50 @@ public class MenuController {
         httpResponseEntity.setCode("200");
         httpResponseEntity.setMessage("OK");
         httpResponseEntity.setData(menus);
+        return httpResponseEntity;
+    }
+
+    @PostMapping("/put")
+    public HttpResponseEntity put(@RequestBody Menu menu, HttpServletRequest request){
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            String ip = IpUtil.getIpAddr(request);
+            int flag = menuService.insert(menu);
+            if (flag > 0) {
+                log.warn(ip + " insert menu " + mapper.writeValueAsString(menu));
+                httpResponseEntity.setCode("200");
+                httpResponseEntity.setMessage("OK");
+            } else {
+                log.warn(ip + " failed to insert menu " + mapper.writeValueAsString(menu));
+                httpResponseEntity.setCode("202");
+                httpResponseEntity.setMessage("error");
+            }
+        } catch (Exception e) {
+            log.warn(e.toString());
+            httpResponseEntity.setCode("500");
+        }
+        return httpResponseEntity;
+    }
+
+    @PostMapping("/update")
+    public HttpResponseEntity update(@RequestBody Menu menu, HttpServletRequest request){
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        try {
+            String ip = IpUtil.getIpAddr(request);
+            int flag = menuService.update(menu);
+            if (flag > 0) {
+                log.warn(ip + " update menu " + mapper.writeValueAsString(menu));
+                httpResponseEntity.setCode("200");
+                httpResponseEntity.setMessage("OK");
+            } else {
+                log.warn(ip + " failed to update menu " + mapper.writeValueAsString(menu));
+                httpResponseEntity.setCode("202");
+                httpResponseEntity.setMessage("error");
+            }
+        } catch (Exception e) {
+            log.warn(e.toString());
+            httpResponseEntity.setCode("500");
+        }
         return httpResponseEntity;
     }
 }
--- src/main/java/live/hardproblem/service/MenuService.java
+++ src/main/java/live/hardproblem/service/MenuService.java
@@ -1,6 +1,7 @@
 package live.hardproblem.service;
 
 import live.hardproblem.dao.ExMenuMapper;
+import live.hardproblem.dao.entity.Food;
 import live.hardproblem.dao.entity.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,28 @@ public class MenuService {
     @Autowired
     ExMenuMapper menuMapper;
 
+    protected void insert_fill(Menu menu) {
+        menu.setId(null);
+        menu.setStatus(null);
+        menu.setCreatedAt(null);
+        menu.setUpdatedAt(null);
+    }
+
+    protected void update_fill(Menu menu) {
+        menu.setUpdatedAt(null);
+    }
+
     public ArrayList<Menu> getAll(boolean all) {
         return (ArrayList<Menu>) menuMapper.getAll(all);
+    }
+
+    public int insert(Menu menu){
+        insert_fill(menu);
+        return menuMapper.insertSelective(menu);
+    }
+
+    public int update(Menu menu){
+        update_fill(menu);
+        return menuMapper.updateByPrimaryKeySelective(menu);
     }
 }
