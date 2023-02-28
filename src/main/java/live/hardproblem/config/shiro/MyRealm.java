6b87dab34e841c3102f24c6566afd1346e9f2a3e
ice22334 <12161471+ice22334@user.noreply.gitee.com>
2023-02-28 20:09:30 +0800

    增加了授权！

--- src/main/java/live/hardproblem/config/shiro/MyRealm.java
+++ src/main/java/live/hardproblem/config/shiro/MyRealm.java
@@ -5,11 +5,16 @@ import live.hardproblem.service.AccountService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class MyRealm extends AuthorizingRealm {
     @Autowired
@@ -18,7 +23,16 @@ public class MyRealm extends AuthorizingRealm {
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         Subject subject = SecurityUtils.getSubject();
-        return null;
+        Account account = (Account) subject.getPrincipal();
+        Set<String> roles = new HashSet<>();
+        if (account.getRole() == 2) {
+            roles.add("admin");
+        } else {
+            roles.add("normal");
+        }
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        info.setRoles(roles);
+        return info;
     }
 
     @Override
