0631798524d9c6c260f8cad9b4c13dfcbfb90fe8
ice22334 <12161471+ice22334@user.noreply.gitee.com>
2023-02-28 20:20:12 +0800

    简单的统一回复机制。

---
+++ src/main/java/live/hardproblem/util/HttpResponseMessage.java
@@ -0,0 +1,12 @@
+package live.hardproblem.util;
+
+public class HttpResponseMessage {
+    public static String goodCode = "200";
+    public static String goodMessage = "OK";
+
+    public static String failedCode = "202";
+    public static String failedMessage = "Failed";
+
+    public static String errorCode = "500";
+    public static String errorMessage = "Error";
+}
